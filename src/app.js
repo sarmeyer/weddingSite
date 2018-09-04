2486c69c2b79e5dbf67c7cee3349b1f831e0dd74
Sarah Meyer <sarah.meyer@formidable.com>
2018-09-04 11:26:30 -0600

    WIP details

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App extends Component {
           </div>
         </div>
         <div id="one" className="main style2 dark right fullscreen">
-          <div className="content box style2">
+          <div className="content box style2 opacity94">
             <h2>The Wedding</h2>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3055.5085759386116!2d-105.28105134934957!3d40.01942327931228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876bec27daad9a33%3A0xd6108c99d20f92f4!2sRembrandt+Yard!5e0!3m2!1sen!2sus!4v1535862592665"
@@ -38,9 +38,11 @@ class App extends Component {
         </div>
         <div id="two" className="main style2 left dark fullscreen">
           <div className="content box style2">
-            <header>
-              <h2>The Welcome Party</h2>
-            </header>
+            <h2>The Welcome Party</h2>
+            <p>Friday, June 28, 2019</p>
+            <p>6-9 PM</p>
+            <p>The Bohemian Biergarten</p>
+            <p>Boulder, CO</p>
           </div>
           <a href="#photos" className="button style2 down anchored">
             Next
@@ -48,15 +50,12 @@ class App extends Component {
         </div>
         <div id="three" className="main style2 right dark fullscreen">
           <div className="content box style2">
-            <header>
-              <h2>Who I Am</h2>
-            </header>
+            <h2>Hotel Info</h2>
             <p>
-              Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore
-              condimentum. Fusce blandit ultrices sapien, in accumsan orci
-              rhoncus eu. Sed sodales venenatis arcu, id varius justo euismod
-              in. Curabitur egestas consectetur magna.
+              For our group rate, please reserve your room before April 25th.
             </p>
+            <a href="https://www.boulderado.com/">Hotel Boulderado</a>
+            <p>2115 13th St, Boulder, CO 80302</p>
           </div>
           <a href="#photos" className="button style2 down anchored">
             Next
@@ -64,15 +63,56 @@ class App extends Component {
         </div>
         <div id="photos" className="main style3 primary">
           <div className="content">
-            <header>
-              <h2>Our Adventures</h2>
-              <p>
-                Lorem ipsum dolor sit amet et sapien sed elementum egestas
-                dolore condimentum. Fusce blandit ultrices sapien, in accumsan
-                orci rhoncus eu. Sed sodales venenatis arcu, id varius justo
-                euismod in. Curabitur egestas consectetur magna vitae.
-              </p>
-            </header>
+            <h2>Meet the Party</h2>
+            <div className="gallery">
+              <article className="from-left">
+                <a href="images/fulls/01.jpg" className="image fit">
+                  <img
+                    src="images/thumbs/01.jpg"
+                    title="The Anonymous Red"
+                    alt=""
+                  />
+                </a>
+              </article>
+              <article className="from-right">
+                <a href="images/fulls/02.jpg" className="image fit">
+                  <img
+                    src="images/thumbs/02.jpg"
+                    title="Airchitecture II"
+                    alt=""
+                  />
+                </a>
+              </article>
+              <article className="from-left">
+                <a href="images/fulls/03.jpg" className="image fit">
+                  <img src="images/thumbs/03.jpg" title="Air Lounge" alt="" />
+                </a>
+              </article>
+              <article className="from-right">
+                <a href="images/fulls/04.jpg" className="image fit">
+                  <img src="images/thumbs/04.jpg" title="Carry on" alt="" />
+                </a>
+              </article>
+              <article className="from-left">
+                <a href="images/fulls/05.jpg" className="image fit">
+                  <img
+                    src="images/thumbs/05.jpg"
+                    title="The sparkling shell"
+                    alt=""
+                  />
+                </a>
+              </article>
+              <article className="from-right">
+                <a href="images/fulls/06.jpg" className="image fit">
+                  <img src="images/thumbs/06.jpg" title="Bent IX" alt="" />
+                </a>
+              </article>
+            </div>
+          </div>
+        </div>
+        <div id="photos" className="main style3 primary">
+          <div className="content">
+            <h2>Our Adventures</h2>
             <div className="gallery">
               <article className="from-left">
                 <a href="images/fulls/01.jpg" className="image fit">
